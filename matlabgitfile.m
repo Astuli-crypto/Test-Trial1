d3e45e7aa39a2ba9bb7f93d6dc48b577d362e695
Cyber-Deck <mananjoshi@thrustworksdynetics.in>
2023-12-17 20:44:05 +0530

    added sub function

--- matlabgitfile.m
+++ matlabgitfile.m
@@ -8,11 +8,16 @@ val1 = T.(columnA)(1);
 val2 = T.(columnB)(1);
 
 sumResult = addNumbers(val1, val2);
+subResult = subNumbers(val1, val2);
 
 
 
 function sumResult = addNumbers(a, b)
     % This function adds two numbers.
-    sumResult = a + b
+    sumResult = a + b;
     
+end
+
+function subResult = subNumbers(a,b)
+    subResult = a-b;
 end
